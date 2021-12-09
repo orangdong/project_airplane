c3447dc9336857b85b23f02d2ec8888fc85d5258
Aviel Naftali <dev.naftaliaviel@gmail.com>
2021-12-09 20:49:28 +0700

    change method name

--- lib/cubit/destination_cubit.dart
+++ lib/cubit/destination_cubit.dart
@@ -8,7 +8,7 @@ part 'destination_state.dart';
 class DestinationCubit extends Cubit<DestinationState> {
   DestinationCubit() : super(DestinationInitial());
 
-  void getDestination() async {
+  void getDestinations() async {
     try {
       emit(DestinationLoading());
       List<DestinationModel> destinations = await DestinationService().getDestinations();
